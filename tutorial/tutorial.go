229c05761c7b264ed03133255c58ee406aca3955
Clara Battesini <clarabattesini@gmail.com>
2024-12-13 20:04:26 -0500

    conditions

--- tutorial/tutorial.go
+++ tutorial/tutorial.go
@@ -5,13 +5,15 @@ import (
 )
 
 func Tutorial() {
-	fmt.Println("* Tutorial *")
-	fmt.Println(variables())
+	fmt.Println("\n* Tutorial *")
+	z, x, c, v := variables()
+	fmt.Println(z, x, c, v, " <- variables")
 	loops()
+	conditions(z, x)
 }
 
 func variables() (int, int, int, int) {
-	fmt.Println("* Variables *")
+	fmt.Println("\n* Variables *")
 	var i, j int = 11, 22
 	k, l := 33, 44
 
@@ -26,7 +28,7 @@ func variables() (int, int, int, int) {
 }
 
 func loops() {
-	fmt.Println("* Loops *")
+	fmt.Println("\n* Loops *")
 
 	sum := 0
 	for i := 0; i < 10; i++ {
@@ -35,9 +37,25 @@ func loops() {
 
 	fmt.Println("Sum:  ", sum)
 
-	for sum < 200 {
+	for sum < 200 { // while loop
 		sum += 10
 	}
 
 	fmt.Println("Sum:  ", sum)
+}
+
+func conditions(x, y int) {
+	defer fmt.Println("End of conditions")
+	fmt.Println("\n* Conditions *")
+
+	if x > y {
+		fmt.Println("x is greater than y")
+	} else {
+		fmt.Println("x is less than or equal to y")
+	}
+
+	if v := x + 10; v > y {
+		fmt.Println("x + 10 is greater than y")
+	}
+
 }
