d3ad98ab7acf4f41e34e5ec542b26961450551d1
Clara Battesini <clarabattesini@gmail.com>
2024-12-15 22:04:20 -0500

    rewritting the database connection

--- internal/infra/database/databaseConnection.go
+++ internal/infra/database/databaseConnection.go
@@ -9,9 +9,17 @@ import (
 
 var db *sql.DB
 
-func InitDB() {
-	var err error
-	db, err = sql.Open("sqlite3", "./village_quest.db")
+type DatabaseConnection interface {
+	Query(statement string, params ...interface{}) (*sql.Rows, error)
+	Close() error
+}
+
+type SqliteAdapter struct {
+	connection *sql.DB
+}
+
+func NewSqliteAdapter() *SqliteAdapter {
+	dbConn, err := sql.Open("sqlite3", "./village_quest.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,8 +27,16 @@ func InitDB() {
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+
+	return &SqliteAdapter{
+		connection: dbConn,
+	}
 }
 
-func GetDB() *sql.DB {
-	return db
+func (s *SqliteAdapter) Query(statement string, params ...interface{}) (*sql.Rows, error) {
+	return s.connection.Query(statement, params...)
+}
+
+func (s *SqliteAdapter) Close() error {
+	return s.connection.Close()
 }
